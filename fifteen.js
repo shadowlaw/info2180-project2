5099461acf722233bf1a8d6767c94e021d8ddb3a
Javed Wright <jdwright870@gmail.com>
2017-10-20 20:07:21 -0500

    Added tile randomization.

--- fifteen.js
+++ fifteen.js
@@ -1,5 +1,7 @@
 window.onload = main;
 
+var blank = ["300px", "300px"];
+
 function initail_state() {
     var puzzle_area = document.getElementById("puzzlearea").childNodes;
     var puzzle_pieces = [];
@@ -30,14 +32,46 @@ function initail_state() {
     return puzzle_pieces
 }
 
-function move_piece(piece, top, left) {
-    piece.style.top = top;
-    piece.style.left = left;
+function is_movable(piece) {
+
+}
+
+function move_piece(piece, animate) {
+    blank_top = piece.style.top;
+    blank_left = piece.style.left;
+
+    if (animate) {
+        $(piece).animate({ "top": blank[0], "left": blank[1] }, "slow");
+    } else {
+        piece.style.top = blank[0];
+        piece.style.left = blank[1];
+    }
+    blank = [blank_top, blank_left];
+}
+
+function random_shuffle(pieces) {
+    var pieceLength = pieces.length;
+    var piece;
+    var rand;
+
+    for (var index = 0; index < pieceLength; index++) {
+        rand = Math.floor(Math.random() * pieces.length);
+        piece = pieces.splice(rand, 1);
+        move_piece(piece[0], false);
+    }
+}
+
+function get_pieces() {
+    return $('.puzzlepiece');
 }
 
 function main() {
-    var puzzle_pieces = initail_state
-    var free_space = ["300", "300"];
+    var puzzle_pieces = initail_state();
+
+    document.getElementById("shufflebutton").onclick = function() {
+        random_shuffle(puzzle_pieces);
+        puzzle_pieces = get_pieces();
+    }
 
 
 }
